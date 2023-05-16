4f87df238f7a8e59f7287fd7149b38e84cf14ef3
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-05-16 13:52:13 +0700

    API versioning

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"golang-web-api/handler"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -15,11 +17,21 @@ func main() {
 	// 	})
 	// })
 
-	router.GET("/", handler.RootHandler)
-	router.GET("/hello", handler.HelloHandler)
-	router.GET("/books/:id/:title", handler.BooksHandler)
-	router.GET("/query", handler.QueryHandler)
-	router.POST("/books", handler.PostBooksHandler)
+	v1 := router.Group("/v1")
+
+	v1.GET("/", handler.RootHandler)
+	v1.GET("/hello", handler.HelloHandler)
+	v1.GET("/books/:id/:title", handler.BooksHandler)
+	v1.GET("/query", handler.QueryHandler)
+	v1.POST("/books", handler.PostBooksHandler)
+
+	v2 := router.Group("/v2")
+	
+	v2.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"content": "Ini adalah API Versioning v2",
+		})
+	})
 
 	router.Run("localhost:8080")
 }
