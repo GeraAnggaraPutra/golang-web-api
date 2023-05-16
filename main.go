8c0b9e0d609890dcac8735e926ca1f3e1553d0fa
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-05-16 12:25:44 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	// router.GET("/", func(ctx *gin.Context) {
+	// 	ctx.JSON(http.StatusOK, gin.H{
+	// 		"name": "Gera Anggara Putra",
+	// 		"bio":  "Software Engineer",
+	// 	})
+	// })
+
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
+
+	router.Run("localhost:8080")
+}
+
+func rootHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"name": "Gera Anggara Putra",
+		"bio":  "Software Engineer",
+	})
+}
+
+func helloHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"title":   "Hello World",
+		"content": "Learning Golang Web API",
+	})
+}
+
+func booksHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+	title := ctx.Param("title")
+	ctx.JSON(http.StatusOK, gin.H{
+		"id": id,
+		"title": title,
+	})
+}
+
+func queryHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+	title := ctx.Query("title")
+	ctx.JSON(http.StatusOK, gin.H{
+		"id": id,
+		"title": title,
+	})
+}
